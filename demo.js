ba3de4161bf5f113f15df817a2191b571e069412
Bylin89 <128812462+Bylin89@users.noreply.github.com>
2023-04-08 13:37:24 +0800

    Update demo.js

--- demo.js
+++ demo.js
@@ -3,5 +3,6 @@ var form = document.getElementById("submit");
 var field1 = form.elements[0];
 var field2 = form.elements[1];
 
-alert(field1);
-alert(field2);
+if(field1 == '123' && field2 == '123'){
+    window.location.href = "flag.html";  
+}
